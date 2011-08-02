5eba11bbb7ba68d8b21fb5c7a766c441d37402ce
Charles Lowell <cowboyd@thefrontside.net>
2011-08-01 23:48:47 -0500

    add the logging build wrapper in the models/ directory.

---
+++ models/log_wrapper.rb
@@ -0,0 +1,18 @@
+
+##
+# Output a message to the console when the build is to start
+# and when it has ended.
+class LoggingWrapper < Jenkins::Tasks::BuildWrapper
+
+  display_name "Logging Build Wrapper"
+
+  # Called some time before the build is to start.
+  def setup(build, launcher, listener, env)
+    listener.log "build will start"
+  end
+
+  # Called some time when the build is finished.
+  def teardown(build, listener, env)
+    listener.log "build finished"
+  end
+end
