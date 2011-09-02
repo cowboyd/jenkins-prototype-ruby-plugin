65f9dab23046b8e75a9301cb09d2a426bf6b19d7
Hiroshi Nakamura <nahi@ruby-lang.org>
2011-09-02 21:35:40 +0900

    Ported test_root_action.rb from the playground
    Just adding TestRootAction as an extension. Lots of TODOs left.

--- lib/models.rb
+++ lib/models.rb
@@ -1,5 +1,14 @@
 # we are supposed to be auto-discovering this
-Jenkins::Model::Descriptor.new("noop_wrapper", LoggingWrapper, self, Java::hudson.tasks.BuildWrapper.java_class).tap do |d|
+Jenkins::Model::Descriptor.new("noop_wrapper", LoggingWrapper, self, Java.hudson.tasks.BuildWrapper.java_class).tap do |d|
   @java.addExtension(d)
   @descriptors[LoggingWrapper] = d
+end
+
+Jenkins::Model::Descriptor.new("test_root_action", TestRootAction, self, Java.hudson.model.RootAction.java_class).tap do |d|
+  @java.addExtension(d)
+  @descriptors[TestRootAction] = d
+end
+
+TestRootAction.new.tap do |action|
+  @java.addExtension(export(action))
 end
---
+++ models/root_action.rb
@@ -0,0 +1,62 @@
+# Ad-hoc extension for jenkins-plugin-runtime
+# TODO: Move to jenkins-plugin-runtime
+module Jenkins
+  module Actions
+    class RootAction
+      include Jenkins::Model::Action
+
+      # TODO: Move to Jenkins::Model::Action?
+      def url_name
+        self.class.name
+      end
+    end
+  end
+end
+
+module Jenkins
+  class Plugin
+    class Proxies
+      class RootAction
+        include Java.hudson.model.RootAction
+        include Java.jenkins.ruby.Get
+        include Jenkins::Plugin::Proxy
+
+        # TODO: is it OK to use snake_case?
+        def display_name
+          @object.display_name
+        end
+
+        def icon_file_name
+          @object.icon
+        end
+
+        def url_name
+          @object.url_name
+        end
+
+        def descriptor
+          @plugin.descriptors[@object.class]
+        end
+
+        def get(name)
+          @object.respond_to?(name) ? @object.send(name) : nil
+        end
+      end
+
+      register Jenkins::Actions::RootAction, RootAction
+    end
+  end
+end
+
+# Plugin part
+class TestRootAction < Jenkins::Actions::RootAction
+  display_name "Test Root Action"
+
+  def icon
+    "gear.png"
+  end
+
+  def url_name
+    "erb"
+  end
+end
