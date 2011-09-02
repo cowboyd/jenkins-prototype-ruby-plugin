860bcec3acf0705e2e5960682bd95df6f4a9fd63
Hiroshi Nakamura <nahi@ruby-lang.org>
2011-09-02 23:53:50 +0900

    Removed unnecessary addExtension for TestRootAction
    TestRootAction is not a Descriptor, so Jenkins won't use this as a
    descriptor, of course.

--- lib/models.rb
+++ lib/models.rb
@@ -4,11 +4,6 @@ Jenkins::Model::Descriptor.new("noop_wrapper", LoggingWrapper, self, Java.hudson
   @descriptors[LoggingWrapper] = d
 end
 
-Jenkins::Model::Descriptor.new("test_root_action", TestRootAction, self, Java.hudson.model.RootAction.java_class).tap do |d|
-  @java.addExtension(d)
-  @descriptors[TestRootAction] = d
-end
-
 TestRootAction.new.tap do |action|
   @java.addExtension(export(action))
 end
