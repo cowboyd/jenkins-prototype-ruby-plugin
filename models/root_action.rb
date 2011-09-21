98136a4b9b5711cc3c285f2ef6257d13412085bf
Hiroshi Nakamura <nahi@ruby-lang.org>
2011-09-21 14:38:38 +0900

    Move RootAction proxy to jenkins-plugin-runtime

--- models/root_action.rb
+++ models/root_action.rb
@@ -1,29 +1,3 @@
-# TODO: Move to jenkins-plugin-runtime
-module Jenkins
-  class Plugin
-    class Proxies
-      class RootAction
-        include Java.hudson.model.RootAction
-        include Jenkins::Plugin::Proxy
-
-        def getDisplayName
-          @object.display_name
-        end
-
-        def getIconFileName
-          @object.icon
-        end
-
-        def getUrlName
-          @object.url_path
-        end
-      end
-
-      register Jenkins::Model::RootAction, RootAction
-    end
-  end
-end
-
 # Plugin part
 
 require 'sinatra/base'
