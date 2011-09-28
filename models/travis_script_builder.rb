0473ace8e8eb06fc13cdcc423c55bd0ed995c684
Hiroshi Nakamura <nahi@ruby-lang.org>
2011-09-28 18:35:37 +0900

    Add Launcher API
    Wrap Launcher as a Ruby's Process.spawn style API.

--- models/travis_script_builder.rb
+++ models/travis_script_builder.rb
@@ -9,11 +9,104 @@ module Jenkins
   end
 
   class Launcher
-    def launch(listener = nil, &block)
+
+    class Proc
+      def initialize(native)
+        @native = native
+      end
+
+      def alive?
+        @native.isAlive()
+      end
+
+      def join
+        @native.join()
+      end
+
+      def kill
+        @native.kill()
+      end
+
+      def stdin
+        @native.getStdin().to_io
+      end
+
+      def stdout
+        @native.getStdout().to_io
+      end
+
+      def stderr
+        @native.getStderr().to_io
+      end
+    end
+
+    # execute([env,] command... [,options]) -> fixnum
+    def execute(*args)
+      spawn(*args).join
+    end
+
+    # spawn([env,] command... [,options]) -> proc
+    def spawn(*args)
+      env, cmd, options = scan_args(args)
       starter = @native.launch()
-      starter.stdout(Jenkins::Plugin.instance.export(listener)) if listener
-      starter.instance_eval(&block)
-      starter.join()
+      # env
+      starter.envs(env)
+      # options
+      if opt_chdir = options[:chdir]
+        starter.pwd(opt_chdir)
+      end
+      if opt_in = options[:in]
+        starter.stdin(opt_in.to_inputstream)
+      end
+      if opt_out = options[:out]
+        if opt_out.is_a?(Jenkins::Model::Listener)
+          starter.stdout(Jenkins::Plugin.instance.export(opt_out))
+        else
+          starter.stdout(opt_out.to_outputstream)
+        end
+      end
+      if opt_err = options[:err]
+        starter.stderr(opt_err.to_outputstream)
+      end
+      # cmd
+      case cmd
+      when Array
+        starter.cmds(cmd)
+      else
+        begin
+          # when we are on 1.432, we can use cmdAsSingleString
+          starter.cmdAsSingleString(cmd.to_s)
+        rescue NoMethodError
+          require 'shellwords'
+          starter.cmds(*Shellwords.split(cmd.to_s))
+        end
+      end
+      Proc.new(starter.start())
+    end
+
+  private
+
+    def scan_args(args)
+      if args.last
+        if Hash === args.last
+          opt = args.pop
+        elsif args.last.respond_to?(:to_hash)
+          opt = args.pop.to_hash
+        end
+      end
+      if args.first
+        if Hash === args.first
+          env = args.shift
+        elsif args.first.respond_to?(:to_hash)
+          env = args.shift.to_hash
+        end
+      end
+      if args.length == 1
+        cmd = args.first
+      else
+        cmd = args
+      end
+      [env || {}, cmd, opt || {}]
     end
   end
 end
@@ -26,6 +119,7 @@ class TravisScriptBuilder < Jenkins::Tasks::Builder
 
   display_name "Travis Builder"
 
+  # TODO: better to use travis-worker instead of re-implementing it?
   def prebuild(build, listener)
     init(build, nil, listener)
     logger.info "Prebuild"
@@ -40,7 +134,7 @@ class TravisScriptBuilder < Jenkins::Tasks::Builder
 
     @gemfile = @config['gemfile'] || 'Gemfile'
     @gemfile = nil unless workspace_file(@gemfile).exists
-    @config['script'] ||= @gemfile ? "bundle install\nbundle exec rake" : 'rake'
+    @config['script'] ||= @gemfile ? "bundle exec rake" : 'rake'
 
     logger.info "Prebuild finished"
   end
@@ -49,15 +143,9 @@ class TravisScriptBuilder < Jenkins::Tasks::Builder
     init(build, launcher, listener)
     logger.info "Build"
 
-    setup_env
-
-    %w{before_script script after_script}.each do |type|
-      next unless @config.key?(type)
-      logger.info "Start #{type}: " + @config[type]
-      scan_multiline_scripts(@config[type]).each do |script|
-        exec(script)
-      end
-    end
+    env = setup_env
+    install_dependencies
+    run_scripts(env)
 
     logger.info "Build finished"
   end
@@ -74,20 +162,40 @@ private
   end
 
   def workspace_file(file)
-    #File.join(@build.workspace, file)
     @build.workspace.child(file)
   end
 
   def setup_env
-    @env = {}
+    env = {}
     if @gemfile
-      @env['BUNDLE_GEMFILE'] = @gemfile
+      env['BUNDLE_GEMFILE'] = @gemfile
     end
     Array(@config['env']).each do |line|
       key, value = line.split(/\s*=\s*/, 2)
-      @env[key] = value
+      env[key] = value
     end
-    logger.info "Additional environment variable(s): #{@env.inspect}"
+    logger.info "Additional environment variable(s): #{env.inspect}"
+    env
+  end
+
+  def install_dependencies
+    if @gemfile
+      # TODO: we should have common gem repository
+      env = {'BUNDLE_PATH' => '.'}
+      script = "bundle install"
+      script += " #{@config['bundler_args']}" if @config['bundler_args']
+      exec(env, script)
+    end
+  end
+
+  def run_scripts(env)
+    %w{before_script script after_script}.each do |type|
+      next unless @config.key?(type)
+      logger.info "Start #{type}: " + @config[type]
+      scan_multiline_scripts(@config[type]).each do |script|
+        exec(env, script)
+      end
+    end
   end
 
   def scan_multiline_scripts(script)
@@ -104,16 +212,9 @@ private
   # 'command execution as a whole String'?
   # http://d.hatena.ne.jp/sikakura/20110324/1300977208 is doing
   # Arrays.asList(str.split(" ")) which should be wrong.
-  def exec(command)
-    logger.info "Launching command: #{command}"
-    workspace = @build.workspace
-    env = @env
-    result = @launcher.launch(@listener) {
-      # You cannot access @var here.
-      pwd(workspace)
-      envs(env)
-      cmds(*Shellwords.split(command))
-    }
+  def exec(env, command)
+    logger.info "Launching command: #{command}, with environment: #{env.inspect}"
+    result = @launcher.execute(command, :chdir => @build.workspace, :out => @listener)
     logger.info "Command execution finished with #{result}"
     raise "command execution failed" if result != 0
   end
