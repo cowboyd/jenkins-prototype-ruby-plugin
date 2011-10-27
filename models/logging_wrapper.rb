1b210a3f786a163c37633574c49bd31863334f39
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-10-27 07:05:03 -0700

    updated to align with my BuildWrapper change

--- models/logging_wrapper.rb
+++ models/logging_wrapper.rb
@@ -7,8 +7,10 @@ class LoggingWrapper < Jenkins::Tasks::BuildWrapper
   display_name "Logging Build Wrapper"
 
   # Called some time before the build is to start.
-  def setup(build, launcher, listener, env)
+  def setup(build, launcher, listener)
     listener.info "build will start"
+
+    return {} # return an 'environment' object that gets passed to the tear down method
   end
 
   # Called some time when the build is finished.
